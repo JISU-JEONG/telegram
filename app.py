2bce63139e1636369ef3f66c1dae7be696f0ff04
JISU-JEONG <jindex2411@naver.com>
2019-07-12 16:18:34 +0900

    연예인 안 닮았네요 다시 설정

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def telegram():
 
             if best.get('confidence') > 0.2:
                 text = f"{best.get('confidence')*100}%만큼 {best.get('value')}를 닮으셨네요~"
+            else:
+                text = '연예인을 닮지는 않았네요...'
         else:
             text = '사람 아닌듯;;'
         api_url = f'{base_url}/sendMessage?chat_id={chat_id}&text={text}'
